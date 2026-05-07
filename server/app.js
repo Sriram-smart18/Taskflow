98147df54ef624498317036705892ae771c12827
sriram <mca111724039105@gmail.com>
2026-05-08 02:07:17 +0530

    Fix Express wildcard route for Node 22

--- server/app.js
+++ server/app.js
@@ -28,11 +28,12 @@ const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.get('*', (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, '../', 'client', 'dist', 'index.html')
-    )
-  );
+  app.use((req, res) => {
+    res.status(404).json({
+      success: false,
+      message: 'Route not found'
+    });
+  });
 } else {
   app.get('/', (req, res) => res.send('API is running...'));
 }
